5a4d87ab7ce78360e84c3227a3ca0a194e6fb30f
Ravi Yadav <yadavravi2801@gmail.com>
2021-05-28 16:58:17 +0530

    add initial css in header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,15 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  width: 100%;
+  min-height: 70px;
+  box-shadow: 0 0 5px 0 rgba(0,0,0,0.3);
+`;
+
 const Header = () => {
-  return <div>Header</div>
+  return <StyledHeader>
+    Header
+  </StyledHeader>
 }
 
 export default Header;
