b261a6ddd9fdee3b2ea79013077f8843e4f21641
Raghav Narang <raghavvnarang@gmail.com>
2021-05-31 05:06:15 +0530

    Added helper paginationQuery for quick firebase pagination query

---
+++ data/firestore/helpers.js
@@ -0,0 +1,28 @@
+import firebase from "@/data/firebase";
+import "firebase/firestore";
+
+const db = firebase.firestore();
+
+export const paginationQuery = async (
+  collection,
+  orderBy,
+  offsetDoc = 0,
+  limit = 10
+) => {
+  const ref = db.collection(collection);
+  let docsRef = "";
+  if (offsetDoc === 0) {
+    docsRef = await ref.orderBy(orderBy).limit(limit).get();
+  } else {
+    docsRef = await ref
+      .orderBy(orderBy)
+      .startAfter(offsetDoc)
+      .limit(limit)
+      .get();
+  }
+
+  const docs = {};
+  docsRef.forEach((doc) => (docs[doc.id] = doc.data()));
+
+  return docs;
+};
--- data/firestore/products.js
+++ data/firestore/products.js
@@ -1,22 +1,9 @@
-import firebase from "@/data/firebase";
-import 'firebase/firestore';
-
-const db = firebase.firestore();
+import { paginationQuery } from "@/data/firestore/helpers";
 
 export const getProducts = async (offset = 0, limit = 10, orderBy = "sku") => {
-  const prodsRef = db.collection("products");
-  if (offset === 0) {
-    return await prodsRef.orderBy(orderBy).limit(limit).get();
-  } else {
-    return await prodsRef.orderBy(orderBy).startAfter(offset).limit(limit).get();
-  }
+  return await paginationQuery("products", orderBy, offset, limit);
 };
 
 export const getCategories = async (offset = 0, limit = 10, orderBy = "id") => {
-  const prodsRef = db.collection("categories");
-  if (offset === 0) {
-    return await prodsRef.orderBy(orderBy).limit(limit).get();
-  } else {
-    return await prodsRef.orderBy(orderBy).startAfter(offset).limit(limit).get();
-  }
+  return await paginationQuery("categories", orderBy, offset, limit);
 };
