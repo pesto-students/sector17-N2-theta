d21856eabbd537423ceb070bb2984380bab438f2
Ravi Yadav <yadavravi2801@gmail.com>
2021-06-03 13:28:44 +0530

    handle cart item count

--- shared/Components/MiniCart/index.js
+++ shared/Components/MiniCart/index.js
@@ -1,33 +1,21 @@
-import { useContext, useEffect } from "react";
-import GlobalContext from "context/GlobalContext"
+import dynamic from 'next/dynamic'
+
+const CartItemsCount = dynamic(() => import('../../Utils/CartItemsCount'), {
+  ssr: false
+});
 
 const MiniCart = (props) => {
   const { dropMenu, handleDropMenu} = props;
-  const { cartItemsCount, setCartItemsCount } = useContext(GlobalContext);
-
+  
   const handleClick = () => {
     handleDropMenu(dropMenu === 'minicart' ? '' : 'minicart');
   }
-
-  const getCartItemsCount = () => {
-    let ItemsInCart = localStorage.getItem('cartItem');
-    return ItemsInCart 
-      ? Object.keys(
-          JSON.parse(ItemsInCart)
-        ).length
-      : 0;
-  }
-
-  useEffect(() => {
-    const itemsCount = getCartItemsCount();
-    setCartItemsCount(itemsCount);
-  }, []);
 
   return <div className="header__action-item mini-cart">
     <div className="clickable" onClick={handleClick}>
       <span className="icon">
         <i className="fa fa-shopping-bag"/>
-        <span className="count">{ cartItemsCount }</span>
+        <CartItemsCount />
       </span>
       <span className="label">
         Bag
