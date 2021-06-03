3c5ee35361e2b093a7504ce670ffd8fc65b357d3
pardeep24 <sharmapardeep971@gmail.com>
2021-06-04 00:17:28 +0530

    Working on Filters

--- src/Catalog/index.js
+++ src/Catalog/index.js
@@ -1,4 +1,5 @@
-import { useProducts } from "@/data";
+import { useCategories, useProducts } from "@/data";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import ProductCard from "../../shared/Components/ProductCard";
@@ -10,30 +11,56 @@ const Catalog = () => {
   const [limit, setLimit] = useState(16);
   const [products, setProducts] = useState({});
 
-  const { data, isLoading, isSuccess } = useProducts(
-    offset,
-    limit,
-    "sku",
-    router.query["category-slug"]
-  );
+  const {
+    data = {},
+    isLoading,
+    isSuccess,
+  } = useProducts(offset, limit, "sku", router.query["category-slug"]);
+
+  const {
+    data: categories = {},
+    isLoading: isCatLoading,
+    isSuccess: isCatSuccess,
+  } = useCategories(0, 20);
+
   useEffect(() => {
     isSuccess && setProducts({ ...products, ...data });
   }, [data]);
-  
+
   const loadMore = () => {
     const productKeys = Object.keys(data);
     const offset = productKeys[productKeys.length - 1];
     setOffset(parseInt(offset));
   };
-
+  
+  // const filterWhere = [];
+  const onFilter =(filter)=>{
+    // filterWhere.push(filter)
+    console.log(filterWhere);
+  }
   return (
     <CatalogStyle>
       <div className="filters">
-        <div class="filter_action">
+        <div className="filter_action">
           <span>Filters</span>
-          <span>Filters</span>
+          <span className="filter_clear">Clear All</span>
         </div>
-      
+        <div className="filter_options">
+          <div className="filter_title">Categories</div>
+          <ul className="">
+            {isSuccess &&
+              !!categories &&
+              Object.keys(categories).map((category) => (
+                <li key={categories[category].id}>
+                  <Link href={`/categories/${categories[category].id}`}>
+                  <a>
+                    <span className="label">{categories[category].name}</span>
+                  </a>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
       <div className="products">
         <div className="heading">
