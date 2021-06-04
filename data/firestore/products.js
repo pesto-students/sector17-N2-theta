dc645ae40d880e66877446481e7f5beab7a6db32
Raghav Narang <raghavvnarang@gmail.com>
2021-06-04 14:07:58 +0530

    Added useProductsBySKU method to fetch multiple products by their SKUs
    Conflicts:
    	data/firestore/products.js
    	data/hooks/use-products.js
    	data/index.js

--- data/firestore/products.js
+++ data/firestore/products.js
@@ -1,7 +1,36 @@
 import { paginationQuery } from "@/data/firestore/helpers";
 
-export const getProducts = async (offset = 0, limit = 10, orderBy = "sku") => {
-  return await paginationQuery("products", orderBy, offset, limit);
+export const getProducts = async ({
+  offset = 0,
+  limit = 10,
+  orderBy = "sku",
+  category = "",
+  sku = [],
+}) => {
+  const where = [];
+
+  /** Categories */
+  if (!!category) {
+    where.push(["category", "==", category]);
+  }
+
+  /** SKUs */
+  let skuArray = sku;
+  if (!!sku) {
+    if (!Array.isArray(sku)) {
+      skuArray = [sku];
+    }
+
+    for (const singleSKU of skuArray) {
+      if (!singleSKU) {
+        continue;
+      }
+
+      where.push(["sku", "==", singleSKU]);
+    }
+  }
+
+  return await paginationQuery("products", orderBy, offset, limit, where);
 };
 
 export const getCategories = async (offset = 0, limit = 10, orderBy = "id") => {
