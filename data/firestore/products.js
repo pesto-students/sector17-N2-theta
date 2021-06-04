066399a7ee912e76c6b8ba8762cbe55472da4645
Raghav Narang <raghavvnarang@gmail.com>
2021-06-04 13:53:51 +0530

    Optimised React Hooks for useProducts to use multiple where conditions

--- data/firestore/products.js
+++ data/firestore/products.js
@@ -1,9 +1,23 @@
-import { paginationQuery } from "@/data/firestore/helpers";
+import { getSingleEntity, paginationQuery } from "./helpers";
 
-export const getProducts = async (offset = 0, limit = 10, orderBy = "sku") => {
-  return await paginationQuery("products", orderBy, offset, limit);
+export const getProducts = async (
+  offset = 0,
+  limit = 10,
+  orderBy = "sku",
+  category = ""
+) => {
+  const where = [];
+  if (!!category) {
+    where.push(["category", "==", category]);
+  }
+
+  return await paginationQuery("products", orderBy, offset, limit, where);
 };
 
 export const getCategories = async (offset = 0, limit = 10, orderBy = "id") => {
   return await paginationQuery("categories", orderBy, offset, limit);
+};
+
+export const getSingleProduct = async (id) => {
+  return await getSingleEntity("products", id);
 };
