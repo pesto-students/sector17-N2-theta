4be5ea1da63ba21b9c7d747cd638342d45d2aeee
Raghav Narang <raghavvnarang@gmail.com>
2021-06-05 17:56:15 +0530

    Added missing async

--- data/hooks/use-products-by-sku.js
+++ data/hooks/use-products-by-sku.js
@@ -4,7 +4,7 @@ import { getProducts } from "../firestore/products";
 const useProductsBySKU = (offset = 0, limit = 10, sku = []) =>
   useQuery(
     ["products", { offset, limit, sku }],
-    () => await getProducts({ offset, limit, orderBy: "sku", sku })
+    async () => await getProducts({ offset, limit, orderBy: "sku", sku })
   );
 
 export default useProductsBySKU;
