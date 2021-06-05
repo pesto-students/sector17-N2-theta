ffcca6566a80b2f5e76bcafc1c039a97a57bd777
Raghav Narang <raghavvnarang@gmail.com>
2021-06-05 17:30:07 +0530

    Added await with API methods inside firestore hooks

--- data/hooks/use-products-by-sku.js
+++ data/hooks/use-products-by-sku.js
@@ -2,8 +2,9 @@ import { useQuery } from "react-query";
 import { getProducts } from "../firestore/products";
 
 const useProductsBySKU = (offset = 0, limit = 10, sku = []) =>
-  useQuery(["products", { offset, limit, sku }], () =>
-    getProducts({ offset, limit, orderBy: "sku", sku })
+  useQuery(
+    ["products", { offset, limit, sku }],
+    () => await getProducts({ offset, limit, orderBy: "sku", sku })
   );
 
 export default useProductsBySKU;
--- data/hooks/use-products.js
+++ data/hooks/use-products.js
@@ -2,15 +2,12 @@ import { useQuery } from "react-query";
 import { getProducts } from "../firestore/products";
 
 const useProducts = (offset = 0, limit = 10, orderBy = "sku", category = "") =>
-  useQuery(["products", { offset, limit, orderBy, category }], () =>
-    getProducts({ offset, limit, orderBy, category })
+  useQuery(
+    ["products", { offset, limit, orderBy, category }],
+    () => await getProducts({ offset, limit, orderBy, category })
   );
 
-<<<<<<< HEAD
-  export const useSingleProduct = (id) =>
-  useQuery(["product", { id }], () => getSingleProduct(id));
-=======
-/** TODO: Add use Single Products method here */
+export const useSingleProduct = (id) =>
+  useQuery(["product", { id }], () => await getSingleProduct(id));
 
->>>>>>> 8cd970b72f2d7e63c20890aa3fa0eb81af48bbd3
 export default useProducts;
