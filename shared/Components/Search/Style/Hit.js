8ef564d9cf8b8aa03d948d0e97fd4b7788378b42
Raghav Narang <raghavvnarang@gmail.com>
2021-06-06 22:14:40 +0530

    Algolia Search Implemented (Search Bar)

---
+++ shared/Components/Search/Style/Hit.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const HitDiv = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const HitTitle = styled.p`
+  margin-left: 15px;
+`;
--- shared/Components/Search/index.js
+++ shared/Components/Search/index.js
@@ -1,8 +1,53 @@
-import Input from "../../Styles/Input";
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Configure,
+} from "react-instantsearch-dom";
+import algoliasearch from "algoliasearch/lite";
+import React, { useCallback, useEffect, useState } from "react";
+import Hit from "./hit";
+import useSearchReducer, { SearchContext } from "./reducer";
+
+const searchClient = algoliasearch(
+  "741E7JC67J",
+  "f9dd23453c512d3465aabedbba514430"
+);
 
 const Search = () => {
+  const openParams = useSearchReducer();
+  const {isOpen, close, open} = openParams;
+
+  const onFocusOutside = useCallback((e) => {
+    const headerSearch = document.getElementById("header-search");
+    if (!headerSearch.contains(e.target)) {
+      close();
+    }
+  });
+
+  useEffect(() => {
+    document.body.addEventListener("click", onFocusOutside);
+
+    return () => document.body.removeEventListener("click", onFocusOutside);
+  }, []);
+
   return (
-      <Input type='text' name='search' placeholder='Search for products' />
+    <SearchContext.Provider value={openParams}>
+      <div id="header-search" className="header__action-item">
+        <InstantSearch
+          indexName="sector17_products"
+          searchClient={searchClient}
+        >
+          <Configure hitsPerPage={10} />
+          <SearchBox onFocus={open} />
+          {isOpen && (
+            <div className="header__dropmenu">
+              <Hits hitComponent={Hit} />
+            </div>
+          )}
+        </InstantSearch>
+      </div>
+    </SearchContext.Provider>
   );
 };
 
