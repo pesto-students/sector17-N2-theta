85457ad06b576f4cc8192ee35c5ac5a5920ee5c2
pardeep24 <sharmapardeep971@gmail.com>
2021-06-07 22:14:18 +0530

    Working on User Profile

---
+++ shared/Components/User/Style/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const UserStyle = styled.div`
+  .profile_pic {
+    width: 30px;
+    img {
+      width: 100%;
+      border-radius: 50%;
+      position: relative;
+      top: 0.5rem;
+    }
+  }
+`;
+
+export default UserStyle;
