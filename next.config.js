6b43a04f7d29503a3238947faf4edeeb7b755c9c
Ravi Yadav <yadavravi2801@gmail.com>
2021-06-17 07:15:28 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -21,4 +21,7 @@ const SentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+module.exports = {
+  target : "serverless"
+}
+//module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
