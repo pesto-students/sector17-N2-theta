2f2d13f04a153439f2e6ffa9aae46fa20b9366c3
Pardeep Sharma <sharmapardeep971@gmail.com>
2021-06-23 03:25:31 +0530

    Conflict resolved in order page

--- src/OrderStatus/index.js
+++ src/OrderStatus/index.js
@@ -1,15 +1,16 @@
+import useOrderStatus from '@/data/hooks/use-orders';
+import { useRouter } from 'next/router';
 import OrderStatusStyle from './Style';
 import Skeleton from 'react-loading-skeleton';
 import GlobalContext from '@/appContext';
 import { useContext, useEffect, useState } from 'react';
 import Address from 'shared/Components/Address';
 import Link from "next/link";
-import saveCartItems from 'shared/Utils/saveCartItems';
-import { useRouter } from 'next/router';
+import { useProductsForOrder } from '@/data/hooks/use-products';
+import useProductsBySKU from '@/data/hooks/use-products-by-sku';
 
 const OrderStatus = () => {
   const router = useRouter();
-  const { currentUser } = useContext(GlobalContext);
   const orderId = router.query['id'];
   const [order, setOrder] = useState({});
   const [subtotal, setSubtotal] = useState([]);
@@ -39,10 +40,6 @@ const OrderStatus = () => {
       setSubtotal(
         subtotalArray && subtotalArray.reduce((acc, val) => acc + val, 0)
       );
-
-      if(currentUser) {
-        saveCartItems({}, currentUser.uid);
-      }
     }
   }, [order]);
 
@@ -66,118 +63,87 @@ const OrderStatus = () => {
           <div className="order">
             <div className="row_group item">
               <div className="product_img">
-                <img src="https://storage.googleapis.com/sector17-chandigarh.appspot.com/1032001_sa.jpg" />
+                <Skeleton height={150} />
               </div>
               <div className="product_info">
                 <div className="name">
-                  Aquarius - Rush Playing Cards - Red/Black/White
+                  <Skeleton count={2} />
                 </div>
                 <div className="details">
-                  <div>
-                    <span className="label">Sku:</span>
-                    <span className="value">1003531</span>
-                  </div>
-                  <div>
-                    <span className="label">Manufacturer:</span>
-                    <span className="value">Aquarius</span>
-                  </div>
-                  <div>
-                    <span className="label">Model:</span>
-                    <span className="value">52290</span>
-                  </div>
+                  <Skeleton count={4} />
                 </div>
               </div>
-              <div className="product_qty">Qty: 5</div>
-              <div className="product_price">Rs. 100</div>
+            </div>
+          </div>
+          <div className="summry">
+            <h2>Order Summery</h2>
+
+            <p>
+              <strong>Subtotal: </strong>
+              <span>
+                <Skeleton />
+              </span>
+            </p>
+            <p>
+              <strong>Discount: </strong>
+              <span>
+                <Skeleton />
+              </span>
+            </p>
+            <p>
+              <strong>Total: </strong>
+              <span>
+                <Skeleton />
+              </span>
+            </p>
+          </div>
+        </div>
+      </OrderStatusStyle>
+    );
+  }
+  if (isError) {
+    return <h1>not Good</h1>;
+  }
+  return (
+    <OrderStatusStyle>
+      {order ? (
+        <div>
+          <h1>Order: #{router.query['id']}</h1>
+          <div className="row_group"></div>
+          <div className="row_group orders-row">
+            <div className="order">
+              <div className="address">
+                <Address disable={true} />
+              </div>
+              <h1>Thank you for your order! <Link href="/">Continue Shopping</Link></h1>
             </div>
             <div className="summry">
               <h2>Order Summery</h2>
 
               <p>
                 <strong>Subtotal: </strong>
-                <span>
-                  <Skeleton />
-                </span>
+                <span>Rs. {subtotal}</span>
               </p>
-              <p>
-                <strong>Discount: </strong>
-                <span>
-                  <Skeleton />
-                </span>
-              </p>
-              <p>
-                <strong>Total: </strong>
-                <span>
-                  <Skeleton />
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </OrderStatusStyle>
-    );
-  }
-  
-  if (isError) {
-    return <h1>not Good</h1>;
-  }
 
-  return (
-    order ? (
-      <OrderStatusStyle>
-          <div>
-            <h1>Order: #{router.query['id']}</h1>
-            <div className="row_group"></div>
-            <div className="row_group orders-row">
-              <div className="order">
-                <div className="address">
-                  {
-                    // eslint-disable-next-line react/jsx-boolean-value
-                    <Address disable={true} />
-                  }
-                </div>
-                <h1>Thank you for your order! <Link href="/">Continue Shopping</Link></h1>
-              </div>
-              <div className="product_info">
-                <div className="name">
-                  Aquarius - Rush Playing Cards - Red/Black/White
-                </div>
-                <div className="details">
-                  <div>
-                    <span className="label">Sku:</span>
-                    <span className="value">1003531</span>
-                  </div>
-                  <div>
-                    <span className="label">Manufacturer:</span>
-                    <span className="value">Aquarius</span>
-                  </div>
-                  <div>
-                    <span className="label">Model:</span>
-                    <span className="value">52290</span>
-                  </div>
-                </div>
-              </div>
-              <div className="product_qty">Qty: 5</div>
-              <div className="product_price">Rs. 100</div>
+              {order && order.couponDiscount && order.couponDiscount.coupon && (
+                <p>
+                  <strong>Discount: </strong>
+                  <span>Rs. {order.couponDiscount.discount}</span>
+                </p>
+              )}
+              {order.total != '' && (
+                <p>
+                  <strong>Total: </strong>
+                  <span>Rs. {order.total}</span>
+                </p>
+              )}
             </div>
           </div>
-          <div className="summry">
-          <h2>Order Summery</h2>
-          <p>
-            <strong>Subtotal: </strong> 
-            <span>Rs. 500</span>
-          </p>
-          <p>
-            <strong>Discount: </strong>
-            <span>Rs. 100</span>
-          </p>
-          <p>
-            <strong>Total: </strong>
-            <span>Rs. 400</span>
-          </p>
         </div>
-      </OrderStatusStyle>
-    ) : null
-  )
+      ) : (
+        <div>There are something wrong nothing found by given Order #ID</div>
+      )}
+    </OrderStatusStyle>
+  );
 };
 export default OrderStatus;
